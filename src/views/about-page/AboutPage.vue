8d75c8df48a2b8f72724872f110f05e1df6ed7b7
JeralSandeeptha <jeral.sandeeptha1@gmail.com>
2025-06-01 21:36:27 +0530

    Added pages templates and fix some bugs

---
+++ src/views/about-page/AboutPage.vue
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<template>
+  <div>
+    <h1>About Page</h1>
+  </div>
+</template>
+
+<style scoped>
+/* Optional styles */
+</style>
---
+++ src/views/home-page/HomePage.vue
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<template>
+  <div>
+    <h1>Home Page</h1>
+  </div>
+</template>
+
+<style scoped>
+/* Optional styles */
+</style>
