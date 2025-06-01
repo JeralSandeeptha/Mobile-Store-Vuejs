d988c22cd22adf00e7bd42bca451fd62b52c112d
JeralSandeeptha <jeral.sandeeptha1@gmail.com>
2025-06-02 01:30:54 +0530

    Added sale banner

--- src/views/home-page/HomePage.vue
+++ src/views/home-page/HomePage.vue
@@ -2,12 +2,14 @@
 import HomeHero from '../../components/home-hero/HomeHero.vue';
 import Navbar from '../../components/navbar/Navbar.vue';
 import HeroBottomBanner from '../../components/hero-bottom-banner/HeroBottomBanner.vue';
+import SaleBanner from '../../components/sale-banner/SaleBanner.vue';
 </script>
 
 <template>
   <Navbar />
   <HomeHero />
   <HeroBottomBanner />
+  <SaleBanner />
 </template>
 
 <style scoped>
